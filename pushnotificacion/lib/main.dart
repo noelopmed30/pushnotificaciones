3090eddb7f2075e77f05d0be590a238bd9229bb4
noelopmed30 <107820607+noelopmed30@users.noreply.github.com>
2022-10-29 10:55:29 -0500

    Update main.dart

--- pushnotificacion/lib/main.dart
+++ pushnotificacion/lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 import 'LoginScreen.dart';
-void main() {
-  runApp( App());
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseMessaging.instance.getToken();
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("FCM Token: $fcmToken");
+  runApp(App());
 }
-class App extends StatelessWidget {
-//TODO: Implement build
+
+class App extends StatelessWidget{
+
+
   @override
   Widget build(BuildContext context) {
+
+    // TODO: implement build
     return MaterialApp(
+
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
   }
+
 }
 
